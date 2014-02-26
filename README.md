06e04e639d09ac293816869edb0c222eb69e9e37
Erick Hitter <ehitter@gmail.com>
2014-02-25 21:15:27 -0800

    capital_P_dangit()

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## Wp Redis Cache
 
-Cache Wordpress using Redis, the fastest way to date to cache Wordpress.
+Cache WordPress using Redis, the fastest way to date to cache WordPress.
 
 ### Requirements
 ------
-* [Wordpress](http://wordpress.org) - CMS framework/blogging system
+* [WordPress](http://wordpress.org) - CMS framework/blogging system
 * [Redis](http://redis.io/) - Key Value in memory caching
 * [Predis](https://github.com/nrk/predis) - PHP api for Redis
 
-### Installation 
+### Installation
 ------
 Install Redis, must have root access to your machine. On debian it's as simple as:
 ```bash
@@ -25,9 +25,9 @@ If you don't have the pecl extension installed it will default to use [Predis](h
 Move the folder wp-redis-cache to the plugin directory and activate the plugin.  In the admin section you can set how long you will cache the post for.  By default it will cache the post for 12 hours.
 Note: This plugin is optional and is used to refresh the cache after you update a post/page
 
-Move the `index-wp-redis.php` to the root/base Wordpress directory.
+Move the `index-wp-redis.php` to the root/base WordPress directory.
 
-Move the `index.php` to the root/base Wordpress directory.  Or manually change the `index.php` to:
+Move the `index.php` to the root/base WordPress directory.  Or manually change the `index.php` to:
 
 ```php
 <?php
@@ -36,16 +36,16 @@ require('index-wp-redis.php');
 ```
 In `index-wp-redis.php` change `$ip_of_your_website` to the IP of your server
 
-*Note: Sometimes when you upgrade Wordpress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
+*Note: Sometimes when you upgrade WordPress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
 
-We do this because Wordpress is no longer in charge of displaying our posts.  Redis will now serve the post if it is in the cache.  If the post is not in the Redis cache it will then call Wordpress to serve the page and then cache it for the next pageload
+We do this because WordPress is no longer in charge of displaying our posts.  Redis will now serve the post if it is in the cache.  If the post is not in the Redis cache it will then call WordPress to serve the page and then cache it for the next pageload
 
 
 ### Benchmark
 ------
 I welcome you to compare the page load times of this caching system with other popular Caching plugins such as [Wp Super Cache](http://wordpress.org/plugins/wp-super-cache/) and [W3 Total Cache](http://wordpress.org/plugins/w3-total-cache/)
 
-With a fresh Wordpress install:
+With a fresh WordPress install:
 
 Wp Super Cache
 ```
--- wp-redis-cache/options.php
+++ wp-redis-cache/options.php
@@ -6,18 +6,18 @@ Version: 1.0
 Author: Benjamin Adams
 Author URI: http://dudelol.com
 
-Cache Wordpress using Redis, the fastest way to date to cache Wordpress.
+Cache WordPress using Redis, the fastest way to date to cache WordPress.
 
 == Description ==
 ## Wp Redis Cache
 ------
-Cache Wordpress using Redis, the fastest way to date to cache Wordpress.
+Cache WordPress using Redis, the fastest way to date to cache WordPress.
 
 Please see [https://github.com/BenjaminAdams/wp-redis-cache](https://github.com/BenjaminAdams/wp-redis-cache) for the latest information and other needed setup files.
 
 ### Requirements
 ------
-* [Wordpress](http://wordpress.org) - CMS framework/blogging system
+* [WordPress](http://wordpress.org) - CMS framework/blogging system
 * [Redis](http://redis.io/) - Key Value in memory caching
 * [Predis](https://github.com/nrk/predis) - PHP api for Redis
 
@@ -31,9 +31,9 @@ On other systems please refer to the [Redis website](http://redis.io/).
 Move the folder wp-redis-cache to the plugin directory and activate the plugin.  In the admin section you can set how long you will cache the post for.  By default it will cache the post for 12 hours.
 Note: This plugin is optional and is used to refresh the cache after you update a post/page
 
-Move the `index-wp-redis.php` to the root/base Wordpress directory.
+Move the `index-wp-redis.php` to the root/base WordPress directory.
 
-Move the `index.php` to the root/base Wordpress directory.  Or manually change the `index.php` to:
+Move the `index.php` to the root/base WordPress directory.  Or manually change the `index.php` to:
 
 ```php
 <?php
@@ -42,16 +42,16 @@ require('index-wp-redis.php');
 ```
 In `index-wp-redis.php` change `$ip_of_your_website` to the IP of your server
 
-*Note: Sometimes when you upgrade Wordpress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
+*Note: Sometimes when you upgrade WordPress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
 
-We do this because Wordpress is no longer in charge of displaying our posts.  Redis will now server the post if it is in the cache.  If the post is not in the Redis cache it will then call Wordpress to serve the page and then cache it for the next pageload
+We do this because WordPress is no longer in charge of displaying our posts.  Redis will now server the post if it is in the cache.  If the post is not in the Redis cache it will then call WordPress to serve the page and then cache it for the next pageload
 
 
 ### Benchmark
 ------
 I welcome you to compare the page load times of this caching system with other popular Caching plugins such as [Wp Super Cache](http://wordpress.org/plugins/wp-super-cache/) and [W3 Total Cache](http://wordpress.org/plugins/w3-total-cache/)
 
-With a fresh Wordpress install:
+With a fresh WordPress install:
 
 Wp Super Cache
 ```
@@ -82,9 +82,9 @@ On other systems please refer to the [Redis website](http://redis.io/).
 Move the folder wp-redis-cache to the plugin directory and activate the plugin.  In the admin section you can set how long you will cache the post for.  By default it will cache the post for 12 hours.
 Note: This plugin is optional and is used to refresh the cache after you update a post/page
 
-Move the `index-wp-redis.php` to the root/base Wordpress directory.
+Move the `index-wp-redis.php` to the root/base WordPress directory.
 
-Move the `index.php` to the root/base Wordpress directory.  Or manually change the `index.php` to:
+Move the `index.php` to the root/base WordPress directory.  Or manually change the `index.php` to:
 
 ```php
 <?php
@@ -93,9 +93,9 @@ require('index-wp-redis.php');
 ```
 In `index-wp-redis.php` change `$ip_of_your_website` to the IP of your server
 
-*Note: Sometimes when you upgrade Wordpress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
+*Note: Sometimes when you upgrade WordPress it will replace over your `index.php` file and you will have to redo this step.  This is the reason we don't just replace the contents of `index-wp-redis.php` with `index.php`.
 
-We do this because Wordpress is no longer in charge of displaying our posts.  Redis will now server the post if it is in the cache.  If the post is not in the Redis cache it will then call Wordpress to serve the page and then cache it for the next pageload
+We do this because WordPress is no longer in charge of displaying our posts.  Redis will now server the post if it is in the cache.  If the post is not in the Redis cache it will then call WordPress to serve the page and then cache it for the next pageload
 
 /*  Copyright 2013  Benjamin Adams  (email : ben@dudelol.com)
 
